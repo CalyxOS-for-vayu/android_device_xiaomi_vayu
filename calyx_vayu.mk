dc849275b6af89d63a73a167e5d186034594482c
ncarvalho99 <natalopes.carvalho@pm.me>
2023-01-11 13:08:21 -0300

    vayu: Update build fingerprints

--- calyx_vayu.mk
+++ calyx_vayu.mk
@@ -21,6 +21,6 @@ PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO X3 Pro
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="vayu_global-user 12 RKQ1.200826.002 V13.0.8.0.SJUMIXM release-keys"
+    PRIVATE_BUILD_DESC="vayu_global-user 12 SKQ1.211006.001 V13.0.9.0.SJUMIXM release-keys"
 
-BUILD_FINGERPRINT := Xiaomi/vayu_global/vayu:12/RKQ1.200826.002/V13.0.8.0.SJUMIXM:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/vayu_global/vayu:12/RKQ1.200826.002/V13.0.9.0.SJUMIXM:user/release-keys
